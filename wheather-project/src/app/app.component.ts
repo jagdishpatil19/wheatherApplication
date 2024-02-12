fe6d527f7ea28415583f7da2d18ec3da827a869c
jagdishpatil19 <patiljagdish471@gmail.com>
2024-02-12 22:06:47 +0530

    small change

--- wheather-project/src/app/app.component.ts
+++ wheather-project/src/app/app.component.ts
@@ -4,35 +4,34 @@ import { ApiCallService } from './services/api-call.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wheather-project';
-search: any=''
-  constructor(private apiCall:ApiCallService){}
+  search: any = '';
+  constructor(private apiCall: ApiCallService) {}
 
-boxHide:boolean=false
-  date:any=new Date().toDateString()
-  time:any=new Date().toLocaleTimeString()
-  wheatherData:any;
- main:any=""
- temprature:any=''
- cityName:any=''
- sys:any=''
- wind:any=''
- base:any=''
+  boxHide: boolean = false;
+  date: any = new Date().toDateString();
+  time: any = new Date().toLocaleTimeString();
+  wheatherData: any;
+  main: any = '';
+  temprature: any = '';
+  cityName: any = '';
+  sys: any = '';
+  wind: any = '';
+  base: any = '';
 
-  showData(){
-    this.apiCall.getApiCAll(this.search).subscribe(res=>{
-       this.wheatherData=res
-        this.main=this.wheatherData.main
-        console.log(this.wheatherData)
-        this.cityName=this.wheatherData.name
-        this.sys=this.wheatherData.sys
-        this.temprature=Math.floor(this.main.temp)
-       this.wind=this.wheatherData.wind
-       this.base=this.wheatherData.base
-       this.boxHide=true
-    })
+   async showData() {
+    this.wheatherData=await this.apiCall.getApiCAll(this.search).toPromise()
+      this.main = this.wheatherData.main;
+      console.log(this.wheatherData);
+      this.cityName = this.wheatherData.name;
+      this.sys = this.wheatherData.sys;
+      this.temprature = Math.floor(this.main.temp);
+      this.wind = this.wheatherData.wind;
+      this.base = this.wheatherData.base;
+      this.boxHide = true;
+    ;
   }
 }
