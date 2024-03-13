5d169c27ef9f98bdcd76152650dec11019a531ed
jagdishpatil19 <patiljagdish471@gmail.com>
2024-03-13 10:05:47 +0530

    maerque tag add

--- wheather-project/src/app/app.component.ts
+++ wheather-project/src/app/app.component.ts
@@ -21,8 +21,9 @@ export class AppComponent {
   sys: any = '';
   wind: any = '';
   base: any = '';
-
-   async showData() {
+  marque:boolean=true
+   async showData(data:any) {
+    this.search=data
     this.wheatherData=await this.apiCall.getApiCAll(this.search).toPromise()
       this.main = this.wheatherData.main;
       console.log(this.wheatherData);
@@ -32,6 +33,12 @@ export class AppComponent {
       this.wind = this.wheatherData.wind;
       this.base = this.wheatherData.base;
       this.boxHide = true;
+if( this.search==''){
+this.marque=true
+}
+else{
+  this.marque=false
+}
     ;
   }
 }
